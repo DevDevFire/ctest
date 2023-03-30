603693bd598cd5d42dc4cb3b1266f5597d054647
DevDevFire <meokhrk@gmail.com>
2023-03-30 14:56:27 +0900

    commit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,8 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('//');
-        echo '通過';
-        $response->assertStatus(200);
+        $response = $this->get('/');
+        echo "\n".'通過';
+        $response->assertStatus(100);
     }
 }
